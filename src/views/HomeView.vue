60371692dd1a5ad4fbd18de3d0d7ecbf810dfbb0
AliveRg <renatgorodov49280@gmail.com>
2023-11-17 10:24:57 +0300

    end vers frontend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,7 +20,7 @@
       </div>
     </section>
     <section
-      class="section_2 mx-auto mb-[150px] flex flex-col-reverse md:flex-row lg:flex-col lg:gap-[120px] xl:max-w-[1196px]"
+      class="section_2 mx-auto mb-[150px] flex flex-col-reverse md:flex-row lg:flex-col lg:gap-[120px] xl:max-w-[1196px] justify-center"
     >
       <div
         class="order-2 lg:order-1 flex flex-col md:flex-col-reverse lg:flex-row items-center lg:items-start justify-between"
@@ -128,7 +128,7 @@
                 Cales <span class="lg:text-[42px] xl:text-[64px]">&</span>
               </p>
               <div
-                class="transitionSetting h-[153px] md:max-w-fit group-hover/buybtn:bg-wight flex justify-between md:justify-start max-h-[442px] rounded-[25px] bg-black p-[11px] md:text-[32px] xl:text-[40px]"
+                class="transitionSetting h-[153px] md:h-auto md:max-w-fit group-hover/buybtn:bg-wight flex justify-between md:justify-start max-h-[442px] rounded-[25px] bg-black p-[11px] md:text-[32px] xl:text-[40px]"
               >
                 <p
                   class="text-wight group-hover/buybtn:text-black"
@@ -138,7 +138,7 @@
                 </p>
                 <div class="flex flex-col justify-between gap-[20px]">
                   <p
-                    class="transitionSetting hidden md:block bg-wight group-hover/buybtn:text-wight w-min rotate-180 rounded-[55px] px-[24px] py-[43px] group-hover/buybtn:scale-105 group-hover/buybtn:bg-black"
+                    class="transitionSetting hidden md:block bg-wight group-hover/buybtn:text-wight w-min rotate-180 rounded-[55px] px-[14px] py-[33px] lg:px-[24px] lg:py-[43px] group-hover/buybtn:scale-105 group-hover/buybtn:bg-black"
                     style="writing-mode: vertical-lr; text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25)"
                   >
                     buy now
@@ -209,18 +209,20 @@
           </div>
         </template>
         <div
-          class="mb-[100px] mt-[37px] flex flex-col md:flex-row items-center gap-[37px] md:overflow-x-auto xl:overflow-y-none md:overflow-y-hidden xl:overflow-x-none mx-auto lg:max-w-[840px] z-10 sm:max-w-[320px] sm:px-[5px] md:max-w-[608px] md:px-[20px] xl:max-w-[1236px] md:h-[170px]"
+          class="mb-[100px] mt-[37px] flex flex-col md:flex-row items-center gap-[37px] md:overflow-x-auto xl:overflow-y-none md:overflow-y-hidden xl:overflow-x-none mx-[20px] md:mx-auto lg:max-w-[840px] z-10 sm:max-w-[520px] sm:px-[5px] md:max-w-[608px] md:px-[20px] xl:max-w-[1236px] md:h-[170px]"
           @mouseenter="rotateEnter()"
           @mouseleave="rotateLeave()"
         >
-          <div class="grid min-w-[176px] grid-cols-4 grid-rows-2 gap-y-[10px] rounded-lg">
-            <div class="h-[10px] w-full rounded-l-md bg-[#F3A352]"></div>
-            <div class="h-[10px] w-full bg-[#F3D3AF]"></div>
-            <div class="h-[10px] w-full bg-[#B14F4F]"></div>
-            <div class="h-[10px] w-full rounded-r-md bg-[#FD865E]"></div>
+          <div
+            class="grid min-w-[93px] md:min-w-[176px] grid-cols-4 grid-rows-2 gap-y-[10px] rounded-lg self-start md:self-auto"
+          >
+            <div class="h-[5px] md:h-[10px] w-full rounded-l-md bg-[#F3A352]"></div>
+            <div class="h-[5px] md:h-[10px] w-full bg-[#F3D3AF]"></div>
+            <div class="h-[5px] md:h-[10px] w-full bg-[#B14F4F]"></div>
+            <div class="h-[5px] md:h-[10px] w-full rounded-r-md bg-[#FD865E]"></div>
 
             <div
-              class="transitionSetting h-[10px] w-full rounded-md bg-black"
+              class="transitionSetting h-[5px] md:h-[10px] w-full rounded-md bg-black"
               :class="{
                 'col-start-1 col-end-1': activeItem == 0,
                 'col-start-2 col-end-2': activeItem == 1,
@@ -230,24 +232,30 @@
             ></div>
           </div>
           <template v-for="cros in cross" :key="cros.id">
-            <div class="flex group/cross" v-show="!cros.active" @click="switchactive(cros.id)">
+            <div
+              class="flex group/cross md:self-auto"
+              :class="cros.id % 2 ? 'self-start' : 'self-end'"
+              v-show="!cros.active"
+              @click="switchactive(cros.id)"
+            >
               <div
-                class="relative h-[126px] w-[126px] rounded-l-md"
+                class="relative h-[104px] md:h-[126px] w-[104px] md:w-[126px] rounded-l-md"
                 :style="'  background-color: #' + cros.color"
               >
                 <img
                   :src="'../src/images/' + cros.img['first']"
                   alt=""
-                  class="top-1/5 absolute -translate-x-6 -rotate-[20deg] scale-[140%]"
+                  class="top-1/5 absolute -translate-x-6 -rotate-[20deg] scale-[120%] md:scale-[140%]"
                 />
               </div>
               <div
-                class="relative h-[126px] w-[195px] rounded-r-md"
+                class="relative h-[104px] md:h-[126px] w-[161px] md:w-[195px] rounded-r-md"
                 :style="'  background-color: #' + cros.colorOpacity"
               >
-                <div class="main_text absolute px-[10px] pt-[5px] text-[20px]">
+                <div class="main_text absolute px-[10px] pt-[5px] text-[15px] md:text-[20px]">
                   <p class="font-semibold uppercase" style="letter-spacing: 2.5px">
                     calse & <br />
+
                     <span class="text-wight -ml-[8px] rounded-[13px] bg-black px-[8px] py-[4px]">
                       {{ cros.name }}</span
                     >
@@ -257,7 +265,7 @@
                 </div>
                 <div class="flex h-full w-full items-end justify-end">
                   <p
-                    class="transitionSetting buyNow m-[10px] p-[4px] group-hover/cross:bg-black group-hover/cross:text-wight"
+                    class="transitionSetting buyNow rounded-[10px] md:rounded-[15px] text-[14px] md:text-[20px] m-[10px] p-[4px] group-hover/cross:bg-black group-hover/cross:text-wight"
                   >
                     buy now
                   </p>
@@ -290,14 +298,14 @@
           />
         </div>
         <div
-          class="lg:max-w-[840px] relative z-10 mx-auto sm:max-w-[320px] sm:px-[5px] md:max-w-[608px] md:px-[20px] xl:max-w-[1236px]"
+          class="lg:max-w-[840px] relative z-10 mx-auto sm:px-[5px] md:max-w-[608px] md:px-[20px] xl:max-w-[1236px]"
         >
           <div
-            class="transitionSetting relative flex w-full justify-end rounded-[31px] backdrop-blur-md"
+            class="transitionSetting relative flex flex-col md:flex-row w-full md:justify-end rounded-[20px] md:rounded-[31px] backdrop-blur-md"
             :class="{ 'bg-[#617d5f30]': activeSlide == 0, 'bg-[#eedbc25c]': activeSlide !== 0 }"
           >
             <p
-              class="transitionSetting uppercase font-monster font-bold md:text-[32px] lg:text-[36px] xl:text-[64px] absolute left-[5%] md:top-[70%] lg:top-1/4 z-20"
+              class="transitionSetting uppercase font-monster font-bold md:text-[32px] lg:text-[36px] xl:text-[64px] absolute left-[5%] top-[10%] md:top-[70%] lg:top-1/4 z-20"
               :class="{ 'text-[#b4c1af]': activeSlide == 0, 'text-[#B14F4F]': activeSlide !== 0 }"
               style="line-height: 138.023%; /* 88.335px */ letter-spacing: 18.56px"
             >
@@ -316,12 +324,12 @@
                 nextEl: next
               }"
               :modules="modules"
-              class="relative"
+              class="relative w-full"
             >
               <swiper-slide ref="mySwiper" v-for="(slide, index) in slides" :key="index">
-                <div class="flex items-center justify-end h-full">
+                <div class="flex m-4 md:m-0 md:items-center justify-end h-full">
                   <img
-                    class="md:w-[285px] lg:w-[400px] xl:w-[567px]"
+                    class="w-[203px] md:w-[285px] lg:w-[400px] xl:w-[567px]"
                     :src="'../src/images/' + slide"
                     alt=""
                   />
@@ -330,7 +338,7 @@
               <div
                 :class="{ 'opacity-0': activeArrowNext2 }"
                 ref="prev"
-                class="transitionSetting absolute flex items-center justify-center md:m-3 md:w-[40px] md:h-[40px] lg:m-10 lg:w-[60px] lg:h-[60px] bg-black/50 hover:bg-wight/50 rounded-full border-[2px] border-black left-0 top-0 z-30"
+                class="transitionSetting absolute flex items-center justify-center m-2 md:m-3 md:w-[40px] md:h-[40px] lg:m-10 lg:w-[60px] lg:h-[60px] bg-black/50 hover:bg-wight/50 rounded-full border-[2px] border-black left-0 top-0 z-30"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -344,7 +352,7 @@
               <div
                 ref="next"
                 :class="{ 'opacity-0': activeArrowNext1 }"
-                class="transitionSetting absolute flex items-center justify-center md:m-3 md:w-[40px] md:h-[40px] lg:m-10 lg:w-[60px] lg:h-[60px] bg-black/50 hover:bg-wight/50 rounded-full border-[2px] border-black right-0 top-0 z-30"
+                class="transitionSetting absolute flex items-center justify-center m-2 md:m-3 md:w-[40px] md:h-[40px] lg:m-10 lg:w-[60px] lg:h-[60px] bg-black/50 hover:bg-wight/50 rounded-full border-[2px] border-black right-0 top-0 z-30"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -358,11 +366,11 @@
             </swiper>
 
             <div
-              class="transitionSetting flex h-full w-[414px] items-start justify-center rounded-r-[31px] py-[45px]"
+              class="transitionSetting flex md:h-full w-full md:w-[414px] items-start justify-center rounded-b-[20px] md:rounded-b-none md:rounded-r-[31px] py-[20px] md:py-[45px]"
               :class="{ 'bg-[#374D358F]': activeSlide == 0, 'bg-[#eedbc282]': activeSlide !== 0 }"
             >
               <div
-                class="font-monster flex flex-col items-center justify-center md:gap-[40px] lg:gap-[70px] xl:gap-24 not-italic text-black"
+                class="font-monster w-full md:w-auto px-[30px] md:px-0 flex flex-row-reverse md:flex-col items-center justify-between md:justify-center md:gap-[40px] lg:gap-[70px] xl:gap-24 not-italic text-black"
               >
                 <div
                   class="flex flex-col justify-center gap-[26px] md:text-[20px] xl:text-[40px] tracking-[11px]"
@@ -374,9 +382,21 @@
                     <div class="h-[10px] w-full bg-[#B14F4F]"></div>
                     <div class="h-[10px] w-full rounded-r-md bg-[#FD865E]"></div>
                   </div>
+                  <div
+                    class="text-wight flex w-full md:hidden justify-between md:gap-[19px] lg:gap-[29px] xl:gap-[37px] text-[15px] lg:text-[16px] xl:text-[24px] uppercase"
+                  >
+                    <div
+                      v-for="(item, index) in size"
+                      :key="index"
+                      class="transitionSetting tracking-normal flex h-[30px] w-[30px] md:h-[27px] md:w-[27px] lg:h-[33px] lg:w-[33px] xl:w-[48px] xl:h-[48px] items-center justify-center rounded-full border-[1px] border-solid border-black hover:bg-wight hover:text-black cursor-pointer"
+                      @click="switchSize(index)"
+                    >
+                      <p>{{ item }}</p>
+                    </div>
+                  </div>
                 </div>
                 <div
-                  class="text-wight flex md:gap-[19px] lg:gap-[29px] xl:gap-[37px] md:text-[15px] lg:text-[16px] xl:text-[24px] uppercase"
+                  class="hidden md:flex text-wight md:gap-[19px] lg:gap-[29px] xl:gap-[37px] md:text-[15px] lg:text-[16px] xl:text-[24px] uppercase"
                 >
                   <div
                     v-for="(item, index) in size"
@@ -389,12 +409,19 @@
                 </div>
                 <div class="flex items-center justify-center">
                   <div
-                    class="font-monster text-wight flex flex-col items-start gap-[51px] lg:text-[20px] xl:text-[24px] tracking-[6px] px-2"
+                    class="font-monster text-wight flex flex-col items-start gap-[15px] md:gap-[51px] lg:text-[20px] xl:text-[24px] tracking-[6px] px-2"
                   >
-                    <div class="flex flex-col gap-[27px]">
+                    <div class="flex flex-col gap-[13px] md:gap-[27px]">
                       <p class="">Торс</p>
-                      <div class="flex items-center justify-center gap-3">
-                        <input type="range" max="120" min="35" step="1" v-model="newItem2" />
+                      <div class="flex items-center justify-center gap-3 w-[156px] md:w-auto">
+                        <input
+                          class="w-[81px]"
+                          type="range"
+                          max="120"
+                          min="35"
+                          step="2"
+                          v-model="newItem2"
+                        />
                         <p
                           class="text-wight font-monster lg:text-[20px] xl:text-[24px] font-medium min-w-fit"
                           style="line-height: 74.523%; /* 17.885px */ letter-spacing: -0.48px"
@@ -405,7 +432,7 @@
                     </div>
                     <div class="flex flex-col gap-[27px]">
                       <p class="">Рукава</p>
-                      <div class="flex items-center justify-center gap-3">
+                      <div class="flex items-center justify-center gap-3 w-[156px] md:w-auto">
                         <input type="range" max="123" min="32" step="1" v-model="newItem" />
                         <p
                           class="text-wight font-monster lg:text-[20px] xl:text-[24px] font-medium min-w-fit"
@@ -416,7 +443,7 @@
                       </div>
                     </div>
                     <div
-                      class="transitionSetting md:w-[160px] lg:w-[233px] bg-wight md:text-[15px] lg:text-[16px] xl:text-[24px] self-center text-black text-center flex items-center justify-center rounded-lg hover:bg-black hover:text-wight"
+                      class="transitionSetting w-[145px] md:w-[160px] lg:w-[233px] bg-wight text-[12px] md:text-[15px] lg:text-[16px] xl:text-[24px] self-center text-black text-center flex items-center justify-center rounded-lg hover:bg-black hover:text-wight"
                     >
                       <p class="font-bold" style="text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25)">
                         make a reservation
@@ -430,10 +457,10 @@
         </div>
       </div>
     </section>
-    <section class="section_5 md:pt-[150px] lg:pt-[200px] bg-[#020202]">
+    <section class="section_5 pt-[50px] md:pt-[150px] lg:pt-[200px] bg-[#020202]">
       <div class="h-screen border-t-[3px] border-t-wight">
         <div
-          class="relative flex justify-end w-[90%] pb-[40px] mx-auto border-[38px] border-t-0 border-solid border-[#101010] rounded-[47px] rounded-t-[0]"
+          class="relative flex flex-col md:flex-row md:justify-end w-[90%] pb-[40px] mx-auto border-[38px] border-t-0 border-solid border-[#101010] rounded-[47px] rounded-t-[0]"
         >
           <swiper
             :slides-per-view="1"
@@ -443,11 +470,11 @@
               nextEl: next2
             }"
             :modules="modules"
-            class="relative w-1/2"
+            class="relative w-full ml-0"
           >
             <swiper-slide ref="mySwiper" v-for="(slide, index) in slider2" :key="index">
               <div
-                class="flex items-start h-full"
+                class="flex items-start w-1/2 h-full"
                 :class="{ 'justify-start': index == 0, 'justify-end': index == 1 }"
               >
                 <img class="md:w-full lg:w-2/3" :src="'../src/images/' + slide" alt="" />
@@ -455,7 +482,7 @@
             </swiper-slide>
           </swiper>
 
-          <div class="relative z-10 w-2/3 p-[15px] flex flex-col gap-[48px] items-center">
+          <div class="relative z-10 w-full md:w-2/3 p-[15px] flex flex-col gap-[48px] items-center">
             <Transition>
               <div v-if="activeSlide2 == 0">
                 <p class="w-full text-start text-[#B4C1AF] lg:text-[20px] xl:text-[28px] font-bold">
@@ -493,7 +520,7 @@
               class="flex flex-col md:flex-row justify-center gap-[32px] md:-translate-x-[17%] lg:translate-x-0 font-monster text-wight lg:text-[20px] xl:text-[24px] tracking-[6px]"
             >
               <div
-                class="transitionSetting w-[233px] bg-wight self-center text-black text-center flex items-center justify-center rounded-lg hover:bg-black hover:text-wight"
+                class="transitionSetting w-[158px] md:w-[233px] bg-wight self-center text-black text-center flex items-center justify-center rounded-lg hover:bg-black hover:text-wight"
               >
                 <p class="font-bold py-[20px]" style="text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25)">
                   TEST IT
@@ -835,7 +862,6 @@ input[type='range']::-webkit-slider-thumb {
 }
 
 .buyNow {
-  border-radius: 15px;
   border: 3px solid #000;
   background: rgba(43, 140, 58, 0.08);
 
@@ -843,7 +869,7 @@ input[type='range']::-webkit-slider-thumb {
 
   text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   font-family: Wellfleet;
-  font-size: 20px;
+
   font-style: normal;
   font-weight: 400;
   line-height: 121.023%;
